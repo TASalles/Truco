84119623486f5d630a892b5d440602edb1990174
TASalles <spinsalles@gmail.com>
2018-11-27 12:11:06 -0200

    Adição DB

--- PROJ1_RA00195406/TRUCO/DBTEST.cs
+++ PROJ1_RA00195406/TRUCO/DBTEST.cs
@@ -10,27 +10,98 @@ namespace TRUCO
 {
     class DBTEST
     {
-        static String dbName = "Tetris.sqlite";
+        #region Propriedades
+        static String dbName = "Truco.sqlite";
         static String sConexao = "Data Source =" + dbName;
         static SQLiteConnection conexao;
+        #endregion
 
+        #region CriaDB
         public static void CreateDB()
         {
             conexao = new SQLiteConnection(sConexao);
             if (!File.Exists(dbName))
             {
                 conexao.Open();
-                CreateTables();
+                CreateTable();
             }
             else
             {
                 conexao.Open();
             }
         }
+        #endregion
 
+        #region CriaTabela
+        public static void CreateTable()
+        {
+            String sSQL = "CREATE TABLE 'TABULEIRO' (";
+            sSQL += " 'LINHA' INTEGER";
+            sSQL += "'COLUNA' INTEGER";
+            sSQL += " 'JOGADOR' INTEGER";
+            sSQL += ");";
+            SQLiteCommand comando = new SQLiteCommand(conexao);
+            comando.CommandText = sSQL;
+            comando.ExecuteNonQuery();
+            CreatePlacar();
+        }
+        #endregion
+
+        #region CriaPlacar
+        public static void CreatePlacar()
+        {
+            SQLiteCommand comando = new SQLiteCommand(conexao);
+            comando.CommandText = "CREATE TABLE 'PLACAR' ('RESULTADO' INTEGER, 'id' INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE); ";
+            comando.ExecuteNonQuery();
+        }
+        #endregion
+
+        #region SelectAllMesa
+        public static SQLiteDataReader SelectAllMesa()
+        {
+            String sSQL = "SELECT LINHA,COLUNA,JOGADOR FROM MESA ";
+            SQLiteCommand comando = new SQLiteCommand(conexao);
+            comando.CommandText = sSQL;
+            SQLiteDataReader dataReader = comando.ExecuteReader();
+
+            return dataReader;
+        }
+        #endregion
+
+        #region InsertMesa
+        // A Corrigir 
+        //public static void InsertMesa()
+        //{
+        //    String sSQL = "INSERT INTO 'TABULEIRO'(LINHA,COLUNA,JOGADOR)";
+        //    sSQL += "VALUES (" + "," + algo + "," + ((int)jogador) + ")";
+        //    SQLiteCommand comando = new SQLiteCommand(conexao);
+        //    comando.CommandText = sSQL;
+        //    comando.ExecuteNonQuery();
+        //
+        //}
+        #endregion
+
+        #region LoadMesa
+        // A Corrigir 2.0
+        public static void LoadMesa()
+        {
+
+
+        }
+        #endregion
+
+        #region DeletAllPlacar
+        public static void DeletAllPlacar()
+        {
+
+        }
+        #endregion
+
+        #region SalvarJogo
         public void SalvarJogo()
         {
 
         }
+        #endregion
     }
 }
